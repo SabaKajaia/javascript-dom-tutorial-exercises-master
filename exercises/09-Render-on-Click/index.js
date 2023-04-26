0c78d622f0c2449e9398cdc6eef2717df4069c25
Saba Kajaia <sabakajaia2@gmail.com>
2023-04-26 13:02:28 +0200

    13 completado

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,13 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
+
 	
+});
+
+let button = document.querySelector("#superDuperButton");
+button.addEventListener("click", function() {
+  let element = document.createElement('div');
+  element.style.backgroundColor = 'yellow';
+  element.innerHTML = 'Hello World';
+  document.body.appendChild(element);
 });
